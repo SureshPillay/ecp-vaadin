fd883bc1ec934fbe810735f0b9a341649ffa09a5
Dennis Melzer <dennis.melzer@compart.com>
2014-09-24 16:25:58 +0200

    Test

--- makeithappen.vaadin.app/src/makeithappen/vaadin/app/internal/VaadinMainUI.java
+++ makeithappen.vaadin.app/src/makeithappen/vaadin/app/internal/VaadinMainUI.java
@@ -4,21 +4,13 @@ import org.eclipse.emf.ecp.view.core.vaadin.ECPFVaadinViewRenderer;
 import org.eclipse.emf.ecp.view.core.vaadin.ECPVaadinView;
 import org.lunifera.runtime.web.vaadin.databinding.VaadinObservables;
 
-import com.compart.dbqp.web.ui.model.ModelFactory;
-import com.compart.dbqp.web.ui.model.WebUi;
 import com.eclipsesource.makeithappen.model.task.TaskFactory;
 import com.eclipsesource.makeithappen.model.task.User;
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 //@PreserveOnRefresh
@@ -27,7 +19,6 @@ import com.vaadin.ui.themes.ValoTheme;
 public class VaadinMainUI extends UI {
 
 	private static final long serialVersionUID = 1L;
-	final static WebUi WEB_UI = ModelFactory.eINSTANCE.createWebUi();
 	final static User USER = TaskFactory.eINSTANCE.createUser();
 
 	@Override
@@ -41,69 +32,8 @@ public class VaadinMainUI extends UI {
 		setPollInterval(1000);
 	}
 
-	//
-	// @Override
-	// protected void init(VaadinRequest request) {
-	// getPage().setTitle("Test Vaadin Valo");
-	// final WebUiConfiguration uiConfiguration = ModelFactory.eINSTANCE.createWebUiConfiguration();
-	// WEB_UI.setConfiguration(uiConfiguration);
-	//
-	// VerticalLayout verticalLayout = new VerticalLayout();
-	// verticalLayout.setMargin(true);
-	// VaadinObservables.activateRealm(UI.getCurrent());
-	// ECPVaadinView ecpVaadinView = ECPFVaadinViewRenderer.INSTANCE.render(WEB_UI);
-	// verticalLayout.addComponent(ecpVaadinView.getComponent());
-	// ECPVaadinView ecpVaadinView1 = ECPFVaadinViewRenderer.INSTANCE.render(uiConfiguration);
-	// verticalLayout.addComponent(ecpVaadinView1.getComponent());
-	//
-	// setContent(verticalLayout);
-	//
-	// setResizeLazy(true);
-	// setPollInterval(1000);
-	// }
-
 	@Override
 	protected void refresh(VaadinRequest request) {
-	}
-
-	protected Component renderControl() {
-		VerticalLayout layout = new VerticalLayout();
-		// layout.setSizeFull();
-		final Table table = new Table();
-		table.setSelectable(true);
-		table.setSizeFull();
-		table.setPageLength(10);
-		table.setCaption("Test");
-		BeanItemContainer<Object> indexedContainer = new BeanItemContainer(Bean.class);
-
-		setVisibleColumns(table, indexedContainer);
-
-		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		horizontalLayout.addStyleName("table-button-toolbar");
-		Button add = new Button();
-		add.addStyleName("table-add");
-		add.addClickListener(new Button.ClickListener() {
-			int count = 1;
-
-			@Override
-			public void buttonClick(ClickEvent event) {
-				Bean bean = new Bean("Mung bean" + this.count, 1452.0);
-				bean.setName1("" + this.count);
-				bean.setName2("test");
-				indexedContainer.addBean(bean);
-
-			}
-		});
-		horizontalLayout.addComponent(add);
-		Button remove = new Button();
-		remove.addStyleName("table-remove");
-		horizontalLayout.addComponent(remove);
-
-		layout.addComponent(horizontalLayout);
-		layout.setComponentAlignment(horizontalLayout, Alignment.TOP_RIGHT);
-		layout.addComponent(table);
-		return layout;
-
 	}
 
 	private void setVisibleColumns(final Table table, BeanItemContainer<Object> indexedContainer) {
