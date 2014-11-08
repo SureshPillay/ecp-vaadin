8976b1b5b8d62d07bafe8f8635236cc0ae230264
Dennis Melzer <dennis.melzer@compart.com>
2014-11-08 21:46:16 +0100

    Add Stub

---
+++ org.eclipse.emf.ecp.view.common.vaadin.test/src/org/eclipse/emf/ecp/view/common/vaadin/test/CustomControlStub.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Dennis Melzer and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dennis - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.view.common.vaadin.test;
+
+import org.eclipse.emf.ecp.view.custom.vaadin.CustomControlVaadinRenderer;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+
+public class CustomControlStub extends CustomControlVaadinRenderer {
+
+	public final String LABEL_TEXT = "labelText";
+	private Button label;
+
+	@Override
+	protected Component render() {
+		this.label = new Button(this.LABEL_TEXT);
+		return this.label;
+	}
+
+}
