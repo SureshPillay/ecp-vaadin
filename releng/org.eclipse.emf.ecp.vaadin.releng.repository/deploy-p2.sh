e9ca30c3aaa39c7238e28f24ac1d66380aa89c4d
SirWayne <melzer.dennis@gmail.com>
2014-12-18 14:30:27 +0100

    Move update Site

--- releng/org.eclipse.emf.ecp.vaadin.releng.repository/deploy-p2.sh
+++ releng/org.eclipse.emf.ecp.vaadin.releng.repository/deploy-p2.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 echo "starting deploy..."
-git config user.name SirWayne
-git config user.email melzer.dennis@gmail.com
+git config user.name ${GIT_USER}
+git config user.email ${GIT_EMAIL}
 rm -rf p2-repo
 git clone https://github.com/SirWayne/ecp-vaadin.git p2-repo 
+echo cd p2-repo
 cd p2-repo
 rm -rf ecp-vaadin-p2/*
 cd ecp-vaadin-p2
+echo `pwd
 cp -r ../../releng/org.eclipse.emf.ecp.vaadin.releng.repository/target/repository/* .
 git add --all .
 git commit -m "Deployed to Github P2"
-git push --force --quiet "https://${GH_TOKEN}@github.com/SirWayne/SirWayne.github.io.git"
+echo Push
+git push --force --quiet "https://${GH_TOKEN}@github.com/SirWayne/SirWayne.github.io.git" > /dev/null 2>&1
+cd ..
+echo Delete
+rm -rf .git/credentials
