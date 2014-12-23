3c7019383ea7332cfe163cbb7a8bd68fc16e1473
SirWayne <melzer.dennis@gmail.com>
2014-12-23 10:04:50 +0100

    Add Vaadin example

--- examples/org.eclipse.emf.ecp.makeithappen.vaadin.sample.app/src/makeithappen/vaadin/app/internal/VaadinMainUI.java
+++ examples/org.eclipse.emf.ecp.makeithappen.vaadin.sample.app/src/makeithappen/vaadin/app/internal/VaadinMainUI.java
@@ -3,13 +3,17 @@ package makeithappen.vaadin.app.internal;
 import org.eclipse.emf.ecp.makeithappen.model.aufgabe.Benutzer;
 import org.eclipse.emf.ecp.makeithappen.model.aufgabe.TaskFactory;
 import org.eclipse.emf.ecp.view.core.vaadin.ECPVaadinView;
+import org.eclipse.emf.ecp.view.core.vaadin.ECPVaadinViewComponent;
 import org.eclipse.emf.ecp.view.core.vaadin.ECPVaadinViewRenderer;
 import org.lunifera.runtime.web.vaadin.databinding.VaadinObservables;
 
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 // @PreserveOnRefresh
@@ -31,6 +35,40 @@ public class VaadinMainUI extends UI {
 		getPage().setTitle("Test Vaadin Valo");
 		VaadinObservables.activateRealm(this);
 		final ECPVaadinView ecpVaadinView = ECPVaadinViewRenderer.INSTANCE.render(USER);
+		setContent(ecpVaadinView.getComponent());
+
+		// see https://vaadin.com/forum#!/thread/8838904
+		// testVaadinForum(ecpVaadinView);
+
+	}
+
+	private void testVaadinForum(final ECPVaadinView ecpVaadinView) {
+		final ECPVaadinViewComponent ecpVaadinView1 = new ECPVaadinViewComponent();
+		final HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+		ecpVaadinView1.setContent(horizontalLayout);
+		horizontalLayout.setSizeFull();
+		horizontalLayout.setSpacing(true);
+		horizontalLayout.setMargin(true);
+
+		final VerticalLayout layout1 = new VerticalLayout();
+		layout1.addStyleName("group");
+		layout1.addComponent(new TextField());
+		layout1.addComponent(new TextField());
+		layout1.addComponent(new TextField());
+		layout1.addComponent(new TextField());
+		layout1.addComponent(new TextField());
+		layout1.addComponent(new TextField());
+
+		final VerticalLayout layout2 = new VerticalLayout();
+
+		layout2.addStyleName("group");
+		layout2.addComponent(new TextField());
+		layout2.addComponent(new TextField());
+		layout2.setHeight(100, Unit.PERCENTAGE);
+
+		horizontalLayout.addComponent(layout1);
+		horizontalLayout.addComponent(layout2);
 		setContent(ecpVaadinView.getComponent());
 	}
 
